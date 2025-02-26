9296b4f9828a8e42291750bdcb4b1c4203ba8649
quiet98k <oscar20040522@gmail.com>
2025-02-25 18:43:15 -0800

    add test cases

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -3,3 +3,13 @@ import pytest
 from foo_bar_baz import foo_bar_baz
 
 #Add testcases Here
+
+def test_foo_bar_baz():
+    assert foo_bar_baz(1) == "1"
+    assert foo_bar_baz(2) == "1 2"
+    assert foo_bar_baz(3) == "1 2 Foo"
+    assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+    assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
+    assert foo_bar_baz(0) == ""
+    assert foo_bar_baz(-1) == ""
+    assert foo_bar_baz(30) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz 16 17 Foo 19 Bar Foo 22 23 Foo Bar 26 Foo 28 29 Baz"
