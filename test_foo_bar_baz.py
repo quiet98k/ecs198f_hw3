b2c28730c9930f633db1d7eddcd5fcfce88e2c34
quiet98k <oscar20040522@gmail.com>
2025-02-25 18:56:18 -0800

    1

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -10,6 +10,10 @@ def test_foo_bar_baz():
     assert foo_bar_baz(3) == "1 2 Foo"
     assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
+    
     assert foo_bar_baz(0) == ""
-    assert foo_bar_baz(-1) == ""
-    assert foo_bar_baz(30) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz 16 17 Foo 19 Bar Foo 22 23 Foo Bar 26 Foo 28 29 Baz"
+    assert foo_bar_baz(-5) == ""
+    
+    assert foo_bar_baz(30) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz 16 17 Foo 19 Bar Foo 22 23 Foo Bar 26 Foo 28 29 Baz" 
+    
+    assert foo_bar_baz(6).count(" ") == 5
