b8e8545d4ef88e734bfc9d98b23820414d3d835c
quiet98k <oscar20040522@gmail.com>
2025-02-25 21:11:37 -0800

    more test

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -12,17 +12,13 @@ def test_basic():
     assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
 
-def test_invalid():    
     assert foo_bar_baz(0) == ""
     assert foo_bar_baz(-5) == ""
 
-def test_long():
     assert foo_bar_baz(30) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz 16 17 Foo 19 Bar Foo 22 23 Foo Bar 26 Foo 28 29 Baz" 
 
-def test_length():    
     assert foo_bar_baz(6).count(" ") == 5
 
-def test_non_integer_input():
     with pytest.raises(TypeError):
         foo_bar_baz("10")
     with pytest.raises(TypeError):
@@ -33,4 +29,9 @@ def test_non_integer_input():
         foo_bar_baz(None)
     with pytest.raises(TypeError):
         foo_bar_baz()
+        
+    assert foo_bar_baz(3) == "1 2 Foo"
+    assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+    assert foo_bar_baz(6) == "1 2 Foo 4 Bar Foo"
+    assert foo_bar_baz(10) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar"
     
